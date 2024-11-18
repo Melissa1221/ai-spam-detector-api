2eed9e7b2cb744118c5ed9f2ca350f6551f8451e
thsergitox <sergiopezoj@gmail.com>
2024-11-18 13:43:19 -0500

    feat: update spam detection prompt to clarify disinterest in software offers

--- src/integrations/groq/GroqSpamChecker.ts
+++ src/integrations/groq/GroqSpamChecker.ts
@@ -2,7 +2,7 @@ import Groq from 'groq-sdk';
 import { SimpleRequestBody, SpamChecker, SpamCheckResult } from '../../SpamChecker';
 
 // It tells the model to response with 1 if the email is spam, phishing, or illegitimate and 0 if it is not
-const PROMPT = '"Determine whether the following email is spam, phishing, or illegitimate. If it is, respond only with 1. If it is not spam, respond only with 0. Do not include any explanations or additional text. Note: We are an association of computer science students and are not interested in purchasing any products or services."'
+const PROMPT = '"Determine whether the following email is spam, phishing, or illegitimate. If it is, respond only with 1. If it is not spam, respond only with 0. Do not include any explanations or additional text. Note: We are an association of computer science students and are not interested in purchasing any products, services or software."'
 
 export class GroqSpamChecker extends SpamChecker {
 
